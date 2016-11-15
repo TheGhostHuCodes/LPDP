b875a96adc2a333c900e8ed3952e96c1fe50b6c7
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-11-14 21:54:17 -0800

    Add a classic singleton example

---
+++ Chapter02-CreatingOnlyOneObjectWithTheSingletonPattern/AClassicSingleton/singleton.py
@@ -0,0 +1,9 @@
+class Singleton():
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls.instance
+
+
+class Child(Singleton):
+    pass
